d39049e9e37b84a428295b449b0dcddba53f1999
totchi1510 <179804262+totchi1510@users.noreply.github.com>
2025-04-16 22:04:19 +0900

    Update argumentations.py

--- argumentations.py
+++ argumentations.py
@@ -1,1 +1,17 @@
+# augmentations.py
+import albumentations as A
 
+# 利用可能な前処理を定義
+AUGMENTATION_OPTIONS = {
+    "HorizontalFlip": A.HorizontalFlip(p=1.0),
+    "Rotate": A.Rotate(limit=30, p=1.0),
+    "Blur": A.Blur(blur_limit=3, p=1.0),
+    "Sharpen": A.Sharpen(p=1.0),
+    "RandomBrightnessContrast": A.RandomBrightnessContrast(p=1.0),
+    "GaussNoise": A.GaussNoise(p=1.0),
+    "HueSaturationValue": A.HueSaturationValue(p=1.0)
+}
+
+# 指定された拡張名リストから、適用する拡張をリストで返す関数
+def get_augmentations(names):
+    return [AUGMENTATION_OPTIONS[name] for name in names if name in AUGMENTATION_OPTIONS]
